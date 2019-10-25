b61d554c0538b1fbc5e7c148084e57272c5189e7
nkosa427 <nkosa427@gmail.com>
2019-10-24 22:21:30 -0700

    added stuff back

--- kosan.buildrooms.c
+++ kosan.buildrooms.c
@@ -12,21 +12,58 @@ struct room{
 	struct room *connections;
 };
 
-void create_rooms(){
+struct room* create_rooms(char namearray[10][9]){
+	int i, rnum;
+	struct room* RP = malloc(sizeof(room));
+	// printf("names:%s\n", namearray[2]);
+		
+	for(i = 0; i < 7; i++){
+		do{
+			rnum = rand() % 10;
+		}while(namearray[rnum] == "NULL");
+		
+		// strcpy(array[i]->name, "namehere");
+		// strcpy(namearray[rnum], "NULL");
+	
+		// strcpy(array[1]->name, "name");
+		// printf("name: %s\n", array[0]->name);
+		// array[0]->type = 1;
+	}
 
+	return RP;
+
+	// printf("type: %d\n", array[0]->type);
 }
 
 int main()
 {
+	int i;
 	char foldername[30];
 	int p = getpid();
 	sprintf(foldername, "kosan.rooms.%d", getpid());
 
-	struct room *array;
-	array = (struct room*) malloc(7 * sizeof(struct room));
+	char namearray[10][9]={
+		"Batcave",
+		"Room 101",
+		"Holodeck",
+		"Narnia",
+		"Hobbiton",
+		"Tatooine",
+		"The John",
+		"McDolans",
+		"Walmart",
+		"Muh Room"
+	};
 
-	strcpy(array[0].name, "hello");
-	printf("name: %s\n", array[0].name);
+	struct room *array;
+	array = create_rooms(namearray);
+
+	// strcpy(array[0].name, "yoyoyo");
+	// create_rooms(&array, namearray);
+
+	// for(i = 0; i < 7; i++){
+	// 	printf("name: %s\n", array[i].name);
+	// }
 
 
 	// scanf("%s", str);
